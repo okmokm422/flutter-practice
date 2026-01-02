f6f66f5cc77667caa7e54bb58f9deb9e8c26921c
okmokm422 <okmokm422@gmail.com>
2026-01-02 17:20:11 +0900

    statelesswidgetの実験ファイルを追加

---
+++ flutter_dev/lib/statelesswidget.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Counter(),
+    ),
+  );
+}
+
+class Counter extends StatelessWidget {
+
+  const Counter({super.key});
+
+  final _count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.red,
+      width: 100,
+      height: 100,
+      child: Center(
+        child: Text(
+          '$_count',
+          textDirection: TextDirection.ltr,
+        ),
+      ),
+    );
+  }
+}
